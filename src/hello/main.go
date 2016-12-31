77f46997af0a95af4ea0108be786c6099dd81ab1
w <w>
2016-12-31 12:46:47 +0530

    implemented struct with recevicer method

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,29 +3,23 @@ package main
 import (
 	"fmt"
 )
-
+type rubyist struct {
+	name string
+	exp int
+}
+func (g rubyist) getPosition() string{
+	if (g.exp > 4){
+	 return "Senior Dev"
+	}else{
+	 return "Beginer Dev"
+	}
+}
 func main() {
-	//  String Array with size 3
-	//	var techStack [3]string
-	//	techStack[0] = "MEAN"
-	//	techStack[1] = "MERN"
-	//	techStack[2] = "React on Rails"
-	//	fmt.Println(techStack)
-	
-	 // Slice declaration 
-	 // var techStack []string
-	  
-	 // techStack  = append(techStack,"MERN")
-	 // techStack = append(techStack,"Nodejs & express")
-	 // techStack = append(techStack,"MEAN ")
-	 //techStack = append(techStack,"socket.io with react")
-	 // fmt.Println(techStack)
-	 
-	 //Declare and initailze slice
-	 var techStack = []string{"Rails","Spring Boot","MEAN","LAMP","MERN"}
-	 //iterate slice with for loop using range
-	 for i:= range(techStack) {
-		fmt.Println(techStack[i])
-	 }
- }
+	matt   := rubyist {name:"Matt",exp:20}
+	robert := rubyist {name:"Robert",exp:8}
+	uzaif  := rubyist {name:"uzaif",exp:1}
+	fmt.Println(matt.getPosition())
+	fmt.Println(uzaif.getPosition())
+	fmt.Println(robert.getPosition())
+}
 
