2db271943ec2fbf207d5b6db2b1d55ac3619a602
uzaif nilger <uzaif313@gmail.com>
2016-12-31 16:35:05 +0530

    Implement pass by references

--- src/hello/main.go
+++ src/hello/main.go
@@ -6,6 +6,7 @@ import (
 type rubyist struct {
 	name string
 	exp int
+	isExpert bool
 }
 func (g rubyist) getPosition() string{
 	if (g.exp > 4){
@@ -14,18 +15,22 @@ func (g rubyist) getPosition() string{
 	 return "Beginer Dev"
 	}
 }
+
 func main() {
 	matt   := rubyist {name:"Matt",exp:20}
 	robert := rubyist {name:"Robert",exp:8}
 	uzaif  := rubyist {name:"uzaif",exp:1}
+	aman  := rubyist {name:"aman",exp:0}
 	//fmt.Println(matt.getPosition())
 	//fmt.Println(uzaif.getPosition())
 	//fmt.Println(robert.getPosition())
-	var devs = []rubyist {uzaif,matt,robert,{"aman",0}}
+	var devs = []rubyist {uzaif, matt, robert, aman}
 	for i:=range(devs){
+		checkExpert(&devs[i])
 		fmt.Println(devs[i])
 	}
-	
+
 }
-
-
+func checkExpert(r *rubyist){
+	r.isExpert = r.exp > 3
+}
