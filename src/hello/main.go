151b66d4e92f1d6780284a8d091a72da73988bfb
w <w>
2016-12-31 11:28:02 +0530

    iterator add with variation

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,35 +2,30 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"os"
-	"errors"
 )
 
 func main() {
-	hourOfDay := time.Now().Hour()
-	greeting,err  := getGreeting(hourOfDay)
-	    if err != nil{	
-		 fmt.Println(err)
-		 os.Exit(1)
-	    }else{
-		 fmt.Println(greeting)
-		}
+	//Simple for loop.
+	//for i:=0; i<5 ; i++{
+		//fmt.Println(i)
+	//}
+	//for loop with variation
+	//isLessThanFive := true
+	//i:= 0
+	//for isLessThanFive {
+	//	if i >= 5{
+	//		isLessThanFive = false
+	//	}
+	//	fmt.Println(i)
+	//	i++
+	//}
+	//	i := 0
+	// for{
+	//	if i >= 5{
+	//		break;
+	//	}
+	//	fmt.Println(i)
+	//	i++
+	//}
 }
 
-func getGreeting(hour int)(string, error) {
-	var msg string
-	if hour < 7 {
-		err := errors.New("Too Early for greet please sleep now")
-		fmt.Println(err)	
-		return msg,err
-	}
-	if hour < 12{
-		msg = "Good Morning"
-	}else if hour < 18 {
-		msg = "Good Afternoon"
-	}else {
-		msg = "Good Evening"
-	}
-	return msg,nil
-}
