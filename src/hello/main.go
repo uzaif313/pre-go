cd7b40f3cb089fd9a6cba9d170aaba2cc463227c
w <w>
2016-12-31 11:10:08 +0530

    Add return error arg and msg

--- src/hello/main.go
+++ src/hello/main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"fmt"
 	"time"
+	"os"
+	"errors"
 )
 
 func main() {
 	hourOfDay := time.Now().Hour()
-	greeting  := getGreeting(hourOfDay)
-	fmt.Println(greeting)	
+	greeting,err  := getGreeting(hourOfDay)
+	    if err != nil{	
+		 fmt.Println(err)
+		 os.Exit(1)
+	    }else{
+		 fmt.Println(greeting)
+		}
 }
 
-func getGreeting(hour int)string{
-	if hour < 12{
-		return "Good Morning"
-	}else if hour < 18 {
-		return "Good Afternoon"
-	}else {
-		return "Good Evening"
+func getGreeting(hour int)(string, error) {
+	var msg string
+	if hour < 7 {
+		err := errors.New("Too Early for greet please sleep now")
+		fmt.Println(err)	
+		return msg,err
 	}
+	if hour < 12{
+		msg = "Good Morning"
+	}else if hour < 18 {
+		msg = "Good Afternoon"
+	}else {
+		msg = "Good Evening"
+	}
+	return msg,nil
 }
