cd763f6a6142f6361512db1d0ef378f711cf084a
uzaif nilger <uzaif313@gmail.com>
2017-01-01 09:09:55 +0530

    create and import custome package

--- src/hello/main.go
+++ src/hello/main.go
@@ -2,35 +2,13 @@ package main
 
 import (
 	"fmt"
+	"./data"
 )
-type rubyist struct {
-	name string
-	exp int
-	isExpert bool
-}
-func (g rubyist) getPosition() string{
-	if (g.exp > 4){
-	 return "Senior Dev"
-	}else{
-	 return "Beginer Dev"
-	}
-}
+
 
 func main() {
-	matt   := rubyist {name:"Matt",exp:20}
-	robert := rubyist {name:"Robert",exp:8}
-	uzaif  := rubyist {name:"uzaif",exp:1}
-	aman  := rubyist {name:"aman",exp:0}
-	//fmt.Println(matt.getPosition())
-	//fmt.Println(uzaif.getPosition())
-	//fmt.Println(robert.getPosition())
-	var devs = []rubyist {uzaif, matt, robert, aman}
-	for i:=range(devs){
-		checkExpert(&devs[i])
-		fmt.Println(devs[i])
+	devList := data.GetDevList();
+	for _,dev := range devList {
+		fmt.Println(dev)
 	}
-
-}
-func checkExpert(r *rubyist){
-	r.isExpert = r.exp > 3
 }
